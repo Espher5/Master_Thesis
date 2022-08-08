876aabee3396ec402ac33a993964e8782c33f5b2
Espher5 <michelangeloesposito24@gmail.com>
2022-08-08 10:10:49 +0200

    add nsga2 test with deap

--- masterThesis/config.py
+++ masterThesis/config.py
@@ -1,28 +1,5 @@
-GENETIC_ALGORITHM = {
-    'population': 500,
-    'generations': 200,
-    'seed': 494948,
-    'crossover_rate': 1,
-    'mutation_rate': 9.4
-}
+FITNESS_WEIGTHS = (1.0, -1.0)
 
-MODEL = {
-    'speed': 9,
-    'map_size': 200,
-    'steering_angle': 12,
-    'min_length': 5,
-    'max_length': 50,
-    'min_angle': 10,
-    'max_angle': 85,
-    'length_step': 1,
-    'angle_step': 5
-}
-
-FILES = {
-    'ga_archive': '.\\GA_archive\\',
-    'ga_conv': '.\\',
-    'schedules': 'roads.json',
-    'models': 'models.csv',
-    'tc_img': '.\\TC_img\\',
-    'tc_file': '.\\TC_file\\'
-}
+CROSSOVER_PROBABILITY = 0.5
+MUTATION_PROBABILITY = 0.2
+GENERATIONS = 40
