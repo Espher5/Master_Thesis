6efc323cf9496bdbc2a6cfb436190f5c14b31971
Michelangelo Esposito <michelangeloesposito24@gmail.com>
2022-12-02 21:52:00 +0100

    giga update

--- legacy/sbst/algorithm/CpsDuplicates.py
+++
@@ -1,9 +0,0 @@
-from pymoo.model.duplicate import ElementwiseDuplicateElimination
-
-
-class CpsDuplicatesElimination(ElementwiseDuplicateElimination):
-    """
-    Class that removes identical individuals in the population
-    """
-    def is_equal(self, a, b):
-        return a.X[0].states == b.X[0].states
